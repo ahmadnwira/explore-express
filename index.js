da078d4d68c2e81cc9aec1f45bcc2dba0104adcb
salah-ahmed <ahmed22502@gmail.com>
2018-08-10 16:09:43 +0200

    parse request body

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express(); // get instance of express server
 
+// view-engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static(path.join(__dirname, 'static'))); // use static files middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+
 // app.verb('path', (request, response)=>{})
 app.get('/', function (req, res) {
-    res.json(data)
+    res.render('home', {page: 'Home', data: data});
 });
 
-app.get('/posts/:slug', function (req, res) {
-    res.send(`${req.params.slug}`);
+app.get('/posts', function (req, res) {
+    res.render('newpost', {page: 'create post'});
 });
 
-// use static files middleware
-app.use(express.static(path.join(__dirname, 'static')));
+app.post('/posts', function (req, res) {
+
+    let new_data = data;
+    const new_post = {title: req.body.title, content: req.body.content};
+
+    new_data.push(new_post);
+    fs.writeFile("./data.json", JSON.stringify(new_data));
+
+    res.render('home', {page:'Home', data:new_data});
+});
 
 app.listen(8080, () => console.log('app listening on port 8080'))
