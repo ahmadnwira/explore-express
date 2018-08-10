fc31538fc89b91c3e07509bbb2a229c6a1452b97
salah-ahmed <ahmed22502@gmail.com>
2018-08-10 07:17:05 +0200

    middleware and json response

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
+const app = express(); // get instance of express server
+
+const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+// app.verb('path', (request, response)=>{})
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.json(data)
 });
 
 app.get('/posts/:slug', function (req, res) {
     res.send(`${req.params.slug}`);
 });
 
+// use static files middleware
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(8080, () => console.log('app listening on port 8080'))
