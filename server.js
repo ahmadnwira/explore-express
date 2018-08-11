9dc6ef101b0aa6d380d1ffabcbbaa530fb07b3b3
salah-ahmed <ahmed22502@gmail.com>
2018-08-11 16:06:50 +0200

    write/read to mongo

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
-const app = express(); // get instance of express server
+
+const app = express();
+
+const data = [{
+    title: 'post-title',
+    content: 'lorem ipsum ...'
+}, {
+    title: 'post-title-again',
+    content: 'lorem ipsum ...'
+}];
 
 const mongoClient = require('mongodb').MongoClient;
 
 const connectionString = 'mongodb://localhost:37017/myDB';
 
 mongoClient.connect(connectionString, (error, db) => {
-    if (error) {console.log("didn't connect");};
-    console.log('connected successfully');
+    if (error) {
+        console.log("didn't connect");
+    };
+
+    const dbo = db.db("myDB");
+
+    const posts = dbo.collection('posts'); // creates collection
+    posts.insert(data, (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    dbo.collection('posts').find({}, (err, docs) => {
+        docs.forEach(doc => {
+            console.log(`Title => ${doc.title}, Content => ${doc.content}`);
+        });
+    });
+
     db.close();
 });
 
